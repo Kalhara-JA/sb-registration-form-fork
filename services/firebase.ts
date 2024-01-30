d542610cd3193d664749d0478bb9b8a3070c4ee8
Kalhara JA <kalharajak.21@uom.lk>
2024-01-30 20:55:57 +0530

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,21 +1,29 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY as string,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN as string,
+//   databaseURL: process.env.FIREBASE_DATABASE_URL as string,
+//   projectId: process.env.FIREBASE_PROJECT_ID as string,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET as string,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID as string,
+//   appId: process.env.FIREBASE_APP_ID as string,
+//   measurementId: process.env.FIREBASE_MEASUREMENT_ID as string
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAmHLI0othpo-51n0sMQesZT8hHm-a6PZ0",
-  authDomain: "test-1-aa922.firebaseapp.com",
-  databaseURL: "https://test-1-aa922-default-rtdb.firebaseio.com/",
-  projectId: "test-1-aa922",
-  storageBucket: "test-1-aa922.appspot.com",
-  messagingSenderId: "290128348540",
-  appId: "1:290128348540:web:a0880384d1b52f67f390f9",
-  measurementId: "G-CWNTN7V56R",
+  apiKey: "AIzaSyAvWFS5693rtp13jixg5bgvwzmlnTgmqsY",
+  authDomain: "ieeesb24.firebaseapp.com",
+  databaseURL: "https://ieeesb24-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ieeesb24",
+  storageBucket: "ieeesb24.appspot.com",
+  messagingSenderId: "911632793518",
+  appId: "1:911632793518:web:a722b28068fd101ec7523f",
+  measurementId: "G-X50CCGD6GN"
 };
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// export const db = getFirestore(app);
 export const db = getDatabase(app);
 export const auth = getAuth(app);
